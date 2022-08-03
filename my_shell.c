95be71767933f7fcbab7e910ae80b7c8ef2d0661
Richielee630 <richielee630@Richie.localdomain>
2022-08-03 14:56:02 -0700

    github push

--- my_shell.c
+++ my_shell.c
@@ -6,6 +6,8 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <signal.h>
+#include <stdbool.h>
 
 #define MAX_INPUT_SIZE 1024
 #define MAX_TOKEN_SIZE 64
@@ -13,6 +15,19 @@
 
 #define clear() printf("\033[H\033[J")
 
+static int FORG_CHILD_PID = 0;
+static int NUMBER_OF_BG_CHILD = 0;
+static pid_t PGID = 0;
+static pid_t SEQ_PID = 0;
+static bool SEQ_PS_STATUS = false;
+
+struct p_type{
+	int type_id;
+};
+
+typedef struct p_type process_t;
+
+static process_t cur_process = {.type_id = 0};
 /* Splits the string by space and returns the array of tokens
 *
 */
@@ -28,8 +43,6 @@ void openHelp()
 		"\n>ls"
 		"\n>exit"
 		"\n>all other general commands available in UNIX shell"
-		//"\n>pipe handling"
-		//"\n>improper space handling"
 		);
 
 	return;
@@ -39,7 +52,8 @@ char **tokenize(char *line)
 {
   char **tokens = (char **)malloc(MAX_NUM_TOKENS * sizeof(char *));
   char *token = (char *)malloc(MAX_TOKEN_SIZE * sizeof(char));
-  int i, tokenIndex = 0, tokenNo = 0;
+  int tokenIndex = 0, tokenNo = 0;
+  size_t i;
 
   for(i =0; i < strlen(line); i++){
 
@@ -73,11 +87,11 @@ int countTokens(char **tokens){
 int execArgs(char** parsed){
 	// Forking a child
 	pid_t pid = fork();
-
+	cur_process.type_id = 0;
 	if (pid == -1) {
 		printf("\nFailed forking child..");
 		return 0;
-	} else if (pid == 0) {
+	} else if (pid == 0) {	
 		if (execvp(parsed[0], parsed) < 0) {
 			printf("Shell: Incorrect command");
 			return 0;
@@ -85,9 +99,17 @@ int execArgs(char** parsed){
 		exit(0);
 		return 1;
 	} else {
-		// waiting for child to terminate
+		FORG_CHILD_PID = (int)pid;
+		///printf("\nchild pid is %d", (int)pid);
 		wait(NULL);
+		FORG_CHILD_PID = 0;
 		return 1;
+	}
+}
+
+void child_continue(int sig){
+	if(sig == SIGCONT){
+		return;
 	}
 }
 
@@ -95,20 +117,29 @@ int exec_backg_Args(char* cmd_w_space[], int cmd_count){
 	
 	int pid; 
 	int i;
-
-	for (i = 0; i < cmd_count; i++){
+	cmd_count = cmd_count;
+	cur_process.type_id = 1;
+	for (i = 0; i < 1; i++){
 		pid = fork();	
 		if(pid == -1){ //fail
-			printf("\nFailed forking child..");
+			return 0;
 		}else if (pid == 0){  //child
+			setpgid(getpid(),PGID); // the child's GPID is same as PID
+			kill(getppid(), SIGCONT);
 			char **parsed_0;
 			parsed_0 = tokenize(cmd_w_space[i]);
-
-			//printf("child fork returned: %d\n", (int)pid);
 			if (execvp(parsed_0[0], parsed_0) < 0) {
 				printf("Shell: Incorrect command");
 				exit(EXIT_SUCCESS);
 			}
+		}else{
+			signal(SIGCONT, child_continue);
+			pause();
+			//PGID = getpgid(pid);
+			if(!PGID){
+				PGID = getpgid(pid);
+			}
+			NUMBER_OF_BG_CHILD += 1;
 		}
 	}
 	return 1;
@@ -117,27 +148,27 @@ int exec_backg_Args(char* cmd_w_space[], int cmd_count){
 int exec_seq_Args(char* cmd_w_space[], int cmd_count){
     int pid;
 	int i;
-
+	cur_process.type_id = 2; 
 	for(i = 0; i < cmd_count; i++){
 
     	pid = fork();
 		if (pid == -1) {
-			printf("\nFailed forking child..");
 			return 0;
 		} else if (pid == 0) {
 			char **parsed_0;
 			parsed_0 = tokenize(cmd_w_space[i]);
-			//if (!ownCmdHandler(parsed_0)){
-				if (execvp(parsed_0[0], parsed_0) < 0) {
-					printf("Shell: Incorrect command");
-					//return 0;
-					exit(EXIT_SUCCESS);
-				}			
-			//}
-			//exit(0);
+			if (execvp(parsed_0[0], parsed_0) < 0) {
+				printf("Shell: Incorrect command");
+				//return 0;
+				exit(EXIT_SUCCESS);
+			}			
 		} else {
-			// waiting for child to terminate
+			SEQ_PID = pid;
 			waitpid(pid, NULL, 0);
+			if(SEQ_PS_STATUS){
+				SEQ_PS_STATUS = false;
+				break;
+			}	
 		}
 	}
 	return 1;
@@ -148,25 +179,19 @@ int exec_Para_Args(char* cmd_w_space[], int cmd_count){
 	int pid;
     //int status;
 	int i;
-
+	cur_process.type_id = 3;
 	for(i = 0; i < cmd_count; i++){
 
     	pid = fork();
 		if (pid == -1) {
-			printf("\nFailed forking child..");
 			return 0;
 		} else if (pid == 0) {
 			char **parsed_0;
 			parsed_0 = tokenize(cmd_w_space[i]);
-			//if (!ownCmdHandler(parsed_0)){
-				if (execvp(parsed_0[0], parsed_0) < 0) {
-					printf("Shell: Incorrect command");
-					//return 0;
-					exit(EXIT_SUCCESS);
-				}
-				//exit(0);			
-			//}
-			//exit(0);
+			if (execvp(parsed_0[0], parsed_0) < 0) {
+				printf("Shell: Incorrect command");
+				exit(EXIT_SUCCESS);
+			}
 		} 
 
 	}
@@ -200,12 +225,21 @@ int ownCmdHandler(char** parsed)
 	switch (switchOwnArg) {
 	case 1:
 		// printf("\nGoodbye\n");
+		if(NUMBER_OF_BG_CHILD){
+			killpg(PGID, SIGKILL);
+			while(waitpid(-1, NULL, WNOHANG) > 0){  //
+				;
+			}
+		}
 		exit(0);
 	case 2:
 		if (parsed[2] != NULL){
 			printf("Shell: Incorrect command");
 		}else{
-			chdir(parsed[1]);
+			// chdir(parsed[1]);
+			if(chdir(parsed[1]) == -1){
+				printf("Shell: Incorrect command");
+			}
 		}
 		return 1;
 	case 3:
@@ -231,83 +265,51 @@ void print_ptr_arr(char* target[]){
 }
 
 int specialExecutionHandler(char** tokens, char* line){
+	// echo 2 && echo 1 && echo 23 
+	// char* line is a char array = {'e','c','h','o',' ','1',' ','&','&',' ','e','c','h','o',' ','2'}
+	//						      = "echo 2 && echo 1 && echo 23"
+	// char** tokens(0) = [['echo', '2']]
+	// char** tokens(1) = [['echo', '1']]
+	// char** tokens(2) = [['echo', '23']]
+
+	// char** commands_arr = {'echo 2 ',' echo 1 ',' echo 23'}
+	
 	char* background_token = "&";
-	//int background_token_int = 38;
-	
 	char* sequential_token = "&&";
-	//int sequential_token_int = 9766;
-	//const char sequential_token = '&&';
-
 	char* parallel_token = "&&&";
-	//int parallel_token_int = 2500134;
-	
 	int tokes_lengths = countTokens(tokens);
-	//printf("tokens number %d\n", tokes_lengths);
-	// char *commands_arr[tokes_lengths];
-	//printf("len: %d",strlen(line));
 	int i = 0;
-	//int j = 0;
-	//int actual_len = 0;
-	// int background_mode = 1;
-	// int sequential_mode = 2;
-	// int parallel_mode = 3;
 
-	if(strstr(line, parallel_token) != NULL){
+	if(strstr(line, parallel_token) != NULL){  //is to check if parallel_token is exsit in line
 		char *commands_arr[tokes_lengths]; 
 		char *token_from_str;
-		//char **tokenized_token;
 
 		token_from_str = strtok(line, parallel_token);
 		while (token_from_str != NULL){
-			//printf("%s\n", token_from_str);
 			commands_arr[i] = token_from_str;
-			//printf("%s\n", commands_arr[i]);
 			token_from_str = strtok(NULL, parallel_token);	
 			i++;  	//i is the real commands counts exept &&
-			//printf("%d ", i);
 		}
-
-		// for (j = 0; j < i; j ++){   //only [0] ~ [i-1] elements in commands_arr are useful
-		// 	//printf("...%s\n", commands_arr[j]);
-		// 	tokenized_token = tokenize(commands_arr[j]);
-		// 	//executeCommand(tokenized_token, commands_arr[j], &parallel_mode);
-		// }
-
-		exec_Para_Args(commands_arr,i);
+		char **test;
+		test = commands_arr;
+		exec_Para_Args(test, i);
 		return 1;
 
 	}else if(strstr(line, sequential_token) != NULL){  //&&
 		char *commands_arr[tokes_lengths];    // this is an array of pointer 
-		// char *commands_arr_ptr = commands_arr;
-		// char (*ptr_ptr)[tokes_lengths];
-		// ptr_ptr = &commands_arr_ptr;
-		//printf("here");
 		char *token_from_str;
-		//char **tokenized_token;
-		//char ***sqe_token_group;
-		//a pointer of a pointer is a pointer to an array of pointers
 
 		token_from_str = strtok(line, sequential_token);
 		while (token_from_str != NULL){
-			//printf("%s\n", token_from_str);
 			commands_arr[i] = token_from_str;
-			//printf("%s\n", commands_arr[i]);
 			token_from_str = strtok(NULL, sequential_token);	
 			i++;  	//i is the real commands counts exept &&
 			//printf("%d ", i);
 		}
-		// for (j = 0; j < i; j ++){
-		// 	//printf("...%s\n", commands_arr[j]);
-		// 	tokenized_token = tokenize(commands_arr[j]);
 
-		// 	//sqe_token_group[j] = tokenized_token;
-
-		// 	//executeCommand(tokenized_token, commands_arr[j], &sequential_mode);
-
-		// 	//printf(" total commands: %s", commands_arr[j]);
-		// }
-
-		exec_seq_Args(commands_arr, i);
+		char **test;
+		test = commands_arr;
+		exec_seq_Args(test, i);
 
 		return 1;
 
@@ -318,50 +320,19 @@ int specialExecutionHandler(char** tokens, char* line){
 		
 		token_from_str = strtok(line, background_token);
 		while (token_from_str != NULL){
-			//printf("%s\n", token_from_str);
 			commands_arr[i] = token_from_str;
-			//printf("%s\n", commands_arr[i]);
 			token_from_str = strtok(NULL, background_token);	
-			//printf("...%s\n",commands_arr[i]);
 			i++;  	//i is the real commands counts exept &&
-			//printf("%d ", i);
 		}
 		
-		// test[0] = commands_arr[0];
-		// printf("...%s, %s",test[0],commands_arr[0]);
-		
-		//print_ptr_arr(commands_arr);
-		// //printf("...exec, i = %d\n", i);
-
-		// for (j = 0; j < i; j ++){   //only [0] ~ [i-1] elements in commands_arr are useful
-		// 	//printf("...%s\n", commands_arr[j]);
-		// 	tokenized_token = tokenize(commands_arr[j]);
-		// 	//executeCommand(tokenized_token, commands_arr[j], &background_mode);
-		// 	//exec_seq_Args(tokenized_token);
-		// }
-
-		exec_backg_Args(commands_arr, i);
+		char **test;
+		test = commands_arr;
+		exec_backg_Args(test, i);
 		return 1;
 		
 	}
 	return 0;
 
-}
-
-void init_shell()
-{
-	clear();
-	printf("\n\n\n\n******************"
-		"************************");
-	printf("\n\n\n\t******MY_SHELL******");
-	printf("\n\n\t-CMPE142 HOWEWORK 3-");
-	printf("\n\n\n\n*******************"
-		"***********************");
-	char* username = getenv("USER");
-	printf("\n\n\nUSER is: @%s", username);
-	printf("\n");
-	sleep(2);
-	clear();
 }
 
 void printDir(){
@@ -390,32 +361,94 @@ int executeCommand(char **tokens, char *line){
 	return 1;
 }
 
+int isForeground() {
+    pid_t pid = tcgetpgrp(STDIN_FILENO);
+    if(pid == -1 /* piped */ || pid == getpgrp() /* foreground */)
+        return (int)pid;
+    /* otherwise background */
+    return 0;
+}
+
+void INThandler(int sig){
+	signal(sig, SIG_IGN);
+	switch (cur_process.type_id)
+	{
+	case 1:  //for background 
+		break;
+
+	case 2: //for sequential
+		// printf("here");
+		SEQ_PS_STATUS = true;
+		kill(SEQ_PID, SIGKILL);
+		break;
+
+	case 3:
+		killpg(getppid(), SIGTERM);
+		break;
+	}
+
+	cur_process.type_id = 0;
+	signal(SIGINT, INThandler);
+
+	// signal(sig, SIG_IGN);
+	// if(FORG_CHILD_PID){
+	// 	printf("\npid = %d killed\n", FORG_CHILD_PID);
+	// 	kill(FORG_CHILD_PID, SIGINT);
+	// }else{
+	// 	signal(SIGINT, INThandler);
+	// }
+
+	// int pid = isForeground();
+	// if (pid && sig == SIGINT){
+	// 	printf("\npid = %d killed\n", pid);
+	// 	kill(pid, SIGINT);
+	// }else{
+	// 	signal(SIGINT, INThandler);
+	// }
+
+
+	//printf("\nConfirm exit? [y/n]");
+	// int ch = getchar();
+	// if (ch == 'y' || ch == 'Y'){
+	// 	printf("\nforeground process killed.\n");
+	// 	exit(0);
+	// }else{
+	// 	signal(SIGINT, INThandler);
+	// 	getchar();
+	// }
+}
+
+// #include <pthread.h>
+// #include <semaphore.h>
+// sem_t binary;
+
 int main(int argc, char* argv[]) {
 	char  line[MAX_INPUT_SIZE];            
 	char  **tokens;              
 	int i;
-	//int execFlag = 0;
-	//execlp("ls", "ls", (char *)NULL);
-
-	// init_shell();
-	// clear();
-
 	FILE* fp;
 	if(argc == 2) {
 		fp = fopen(argv[1],"r");
-		if(fp < 0) {
+		if(fp == NULL) {
 			printf("File doesn't exists.");
 			return -1;
 		}
 	}
 
-	while(1) {		
+	while(1) {
+		signal(SIGTERM, SIG_IGN);
 
+		if (signal(SIGINT, INThandler) == SIG_ERR){
+			printf("\ncan't catch SIGINT\n"); 
+		} 
+  			
 		while(waitpid(-1, NULL, WNOHANG) > 0){  //
+			NUMBER_OF_BG_CHILD -= 1;
+			if(!NUMBER_OF_BG_CHILD){
+				PGID = 0;
+			}
 			printf("Shell: Background process finished");
 		}	
-		/* BEGIN: TAKING INPUT */
-		// printDir();
 
 		bzero(line, sizeof(line));
 		if(argc == 2) { // batch mode
@@ -429,9 +462,6 @@ int main(int argc, char* argv[]) {
 			getchar();
 		}
 
-		// printf("Command entered: %s (remove this debug output later)\n", line);
-		
-		/* END: TAKING INPUT */
 
 		line[strlen(line)] = '\n'; //terminate with new line
 		tokens = tokenize(line);
@@ -440,13 +470,11 @@ int main(int argc, char* argv[]) {
 			continue;
 		}
 
-		//printf("...clear mem\n");
-		// Freeing the allocated memory	
 		for(i=0;tokens[i]!=NULL;i++){
 			free(tokens[i]);
 		}
 		free(tokens);
-
+		
 	}
 	return 0;
 }
